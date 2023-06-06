e3c6b72b406976c780d338a4e7dc50def6c34952
prynskaf <prince.kaf12@gmail.com>
2023-06-06 11:28:38 +0200

    updated

---
+++ Introduction/script.js
@@ -0,0 +1,27 @@
+// page title
+let pageTItle =  document.querySelector("#title");
+console.log(pageTItle.textContent);
+
+
+// adding font and color to the body
+ let font = "Arial";
+ let color = "rgb(100, 120, 80)";
+
+ document.body.style.fontFamily = font;
+ document.body.style.background = color; 
+ console.log(color , font);
+
+
+
+ //finding the children Elements
+// let contextElement = document.getElementById("content");
+// let ulElements = contextElement.querySelectorAll("ul");
+// let liElements = contextElement.querySelectorAll("li");
+
+console.log(ulElements.inner);
+console.log(liElements);
+
+
+
+
+
