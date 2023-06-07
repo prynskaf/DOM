d6f34267c2935c4ce1ae1c3360c149675bb0880d
prynskaf <prince.kaf12@gmail.com>
2023-06-07 16:05:58 +0200

    updated

--- create/script.js
+++ create/script.js
@@ -1,14 +1,20 @@
 // Array of learners' names
-const learners = ["Learner 1", "Learner 2", "Learner 3", "Learner 4", "Learner 5"];
+const learners = [
+  "Learner 1",
+  "Learner 2",
+  "Learner 3",
+  "Learner 4",
+  "Learner 5",
+  "Learner 6",
+  "Learner 7",
+  "Learner 8",
+  "Learner 9",
+  "Learner 10",
+];
 
 // Function to generate a random background color
 function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 // Function to create a new section for each learner
@@ -16,10 +22,7 @@ function createSection(name) {
   const section = document.createElement("section");
   const color = getRandomColor();
   section.style.backgroundColor = color;
-
-  // Determine text color based on background color
-  const isDark = colorBrightness(color) < 128;
-  section.style.color = isDark ? "white" : "black";
+  section.style.color = colorBrightness(color) < 128 ? "white" : "black";
 
   const paragraph = document.createElement("p");
   paragraph.textContent = name;
@@ -30,29 +33,18 @@ function createSection(name) {
 
 // Function to calculate color brightness
 function colorBrightness(color) {
-  const rgb = parseInt(color.substring(1), 16);
-  const r = (rgb >> 16) & 0xff;
-  const g = (rgb >> 8) & 0xff;
-  const b = (rgb >> 0) & 0xff;
+  const [r, g, b] = color.match(/\w\w/g).map(x => parseInt(x, 16));
   return (r * 299 + g * 587 + b * 114) / 1000;
 }
 
-// Function to shuffle the array using the Fisher-Yates algorithm
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+// Shuffle the array of learners using the Fisher-Yates algorithm
+learners.sort(() => Math.random() - 0.5);
 
 // Get the <article> element
 const article = document.querySelector("article");
 
-// Shuffle the array of learners
-shuffleArray(learners);
-
 // Create a section for each learner and append it to the article
-learners.forEach(function (learner) {
+learners.forEach(learner => {
   const section = createSection(learner);
   article.appendChild(section);
 });
