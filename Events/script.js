6776b26f24922d9ee41dcd0839760102b9c833cd
prynskaf <prince.kaf12@gmail.com>
2023-06-09 17:45:22 +0200

    updated

--- Events/script.js
+++ Events/script.js
@@ -1,43 +1,59 @@
+// Selecting all  the necessary elements from the DOM
 const displayedsquareWrapper = document.querySelector('.displayedsquare-wrapper');
 const log = document.querySelector('.log');
 const ul = document.querySelector('ul');
 const body = document.body;
 
+// Storing  the initial time when the page loads
 const _initTime = Date.now();
 
+// Calculating and format the elapsed time
 const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's';
 };
 
+// Handle the click event on the generated squares
 const clickOnSquare = (e) => {
   const color = e.target.classList[1];
   alert(`Clicked square color: ${color}`);
   logAction(`Clicked on ${color} square`);
 };
 
+// Add click event listeners to the action squares
 const actionSquares = document.querySelectorAll('.actionsquare');
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare);
 }
 
+// Handle the keypress event on the body element
 const handleKeyPress = (e) => {
+  // Check if the Space key was pressed
   if (e.code === 'Space') {
     changePageBackgroundColor();
     logAction('Spacebar hit');
-  } else if (e.key === 'l') {
+  }
+  // Check if the 'l' key was pressed
+  else if (e.key === 'l') {
     clearLog();
     logAction('Log cleared');
-  } else if (e.key === 's') {
+  }
+  // Check if the 's' key was pressed
+  else if (e.key === 's') {
     clearSquares();
     logAction('Squares cleared');
   }
 };
 
+// Add the keypress event listener to the body element
+body.addEventListener('keypress', handleKeyPress);
+
+// Change the background color of the whole page to a random color
 const changePageBackgroundColor = () => {
   const randomColor = getRandomColor();
   body.style.backgroundColor = randomColor;
 };
 
+// Generate a random color in hexadecimal format
 const getRandomColor = () => {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -47,14 +63,21 @@ const getRandomColor = () => {
   return color;
 };
 
+// Clear the log by removing all li elements
 const clearLog = () => {
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
 };
 
+// Clear the generated squares by removing their content
 const clearSquares = () => {
   displayedsquareWrapper.innerHTML = '';
 };
 
-const
+// Log an action by adding a new li element to the log
+const logAction = (action) => {
+  const li = document.createElement('li');
+  li.textContent = `${action} (${getElapsedTime()})`;
+  ul.appendChild(li);
+};
---
+++ dark-kitchen-porject/script.js
@@ -0,0 +1,59 @@
+const categoryButtons = document.querySelectorAll('.categoryBtn');
+const foodCards = document.querySelectorAll('.card');
+const addToCartButtons = document.querySelectorAll('.addToCartBtn');
+const cartItems = document.getElementById('cartItems');
+const totalAmount = document.getElementById('totalAmount');
+const darkModeToggle = document.getElementById('darkModeToggle');
+const  hamMenu =  document.querySelector('.header__close');
+
+const body = document.body;
+
+let currentTotal = 0; // Variable to store the current total amount
+
+// Filter food items based on category
+categoryButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.getAttribute('data-category');
+    foodCards.forEach(card => {
+      if (category === 'all' || card.getAttribute('data-category') === category) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
+
+// Add item to the cart
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const card = button.parentNode;
+    const itemName = card.querySelector('h2').textContent;
+    const itemPrice = parseFloat(card.querySelector('p').textContent.slice(1));
+    const cartItem = document.createElement('li');
+    cartItem.textContent = `${itemName} - $${itemPrice.toFixed(2)}`;
+    cartItems.appendChild(cartItem);
+
+    updateTotalAmount(itemPrice);
+  });
+});
+
+// Update the total amount in the cart
+function updateTotalAmount(price) {
+  currentTotal += price;
+  totalAmount.textContent = `Total: $${currentTotal.toFixed(2)}`;
+}
+
+// Toggle dark mode
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
+
+
+
+//menu icon toggle
+hamMenu.addEventListener('click' , () => {
+    hamMenu.style.display = 'none';
+    hamMenu.style.transition = "all 0.3s ease-in"
+})
